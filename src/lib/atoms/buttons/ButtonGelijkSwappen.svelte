482b68fffd1f230440033665accd7cedb784e2d6
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2024-02-11 20:34:29 +0100

    Button: tekst centreren
    Een van de bugs die nog opgelost moet worden is het netjes centreren van de tekst van de button. Ik ben begonnen met deze bug te verbeteren. Ik heb de tekst een p gegeven. Ik heb daarna de p gestijld. Ik ben achtergekomen dat alle buttons  icon (svg) niet hetzelfde grote hebben. Dus dit moet ik nog alleen oplossen.

--- src/lib/atoms/buttons/ButtonGelijkSwappen.svelte
+++ src/lib/atoms/buttons/ButtonGelijkSwappen.svelte
@@ -1,73 +1,75 @@
-
-
-<a href='/stekjes_overzicht'>
-<button type="button">
-    Gelijk Swappen
-
+<a href="/stekjes_overzicht">
+  <button type="button">
+    <p>Gelijk Swappen</p>
     <svg
-            width="20"
-            height="21"
-            viewBox="0 0 20 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-        <path
-                d="M17.927 7.57075L4.41288 0.355913C2.41452 -0.710956 0 0.737063 0 3.00238V17.1041C0 19.3443 2.36618 20.7942 4.36214 19.777L17.8763 12.8901C20.0368 11.7891 20.0661 8.71276 17.927 7.57075Z"
-                fill=""
-        />
+      <path
+        d="M17.927 7.57075L4.41288 0.355913C2.41452 -0.710956 0 0.737063 0 3.00238V17.1041C0 19.3443 2.36618 20.7942 4.36214 19.777L17.8763 12.8901C20.0368 11.7891 20.0661 8.71276 17.927 7.57075Z"
+        fill=""
+      />
     </svg>
-</button>
+  </button>
 </a>
 
 <style>
+  button {
+    position: absolute;
+    background-color: var(--button-color);
+    color: var(--text-color);
+    font-family: var(--main-font);
+    border: none;
+    border-radius: var(--bradius);
+    padding: 0.4rem 0.9rem 0.6rem 0.9rem;
+    font-size: 1rem;
+    transition: 0.2s;
+    cursor: pointer;
+    z-index: 999;
+    left: 28%;
+    top: 60%;
+  }
+
+  button:hover {
+    background-color: var(--text-color);
+    color: orange;
+  }
+
+  button:hover svg {
+    fill: orange;
+  }
+
+  svg {
+    margin-left: 5px;
+    transform: translateY(4px) scale(0.9);
+    fill: var(--text-color);
+  }
+
+  p {
+    display: inline-block;
+    text-align: center;
+    vertical-align: text-top;
+    line-height: 0.9em;
+  }
+
+  /* MEDIA TABLET*/
+  @media screen and (min-width: 48rem) {
     button {
-        position: absolute;
-        background-color: var(--button-color);
-        color: var(--text-color);
-        font-family: var(--main-font);
-        border: none;
-        border-radius: var(--bradius);
-        padding: 0.4rem 0.9rem .6rem .9rem;
-        font-size: 1rem;
-        transition: 0.2s;
-        cursor: pointer;
-        /* visibility: hidden; */
-        z-index: 999;
-        left: 28%;
-        top: 60%;
+      top: 60%;
+      left: 38%;
+      margin-top: 0.5rem;
+      z-index: 2;
     }
+  }
 
-    button:hover {
-        background-color: var(--text-color);
-        color: orange;
+  /*MEDIA QUERY DESKTOP */
+  @media screen and (min-width: 64rem) {
+    button {
+      top: 70%;
+      left: 45%;
     }
-
-    button:hover svg {
-        fill: orange;
-    }
-
-    svg {
-        margin-left: 5px;
-        transform: translateY(4px) scale(0.9);
-        fill: var(--text-color);
-    }
-
-    /* MEDIA TABLET*/
-    @media screen and (min-width: 48rem) {
-        button {
-            top: 60%;
-            left: 38%;
-            margin-top: 0.5rem;
-            z-index: 2;
-        }
-    }
-
-    /*MEDIA QUERY DESKTOP */
-    @media screen and (min-width: 64rem) {
-        button {
-            top: 70%;
-            left:45%;
-        }
-    }
-
+  }
 </style>
