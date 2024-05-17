3dc41175aa8a447fd37eb6086594929ef7e6f992
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2024-05-17 11:10:08 +0200

    :sparkles: Made agendaCard responsive

--- src/lib/molecules/agenda-card.svelte
+++ src/lib/molecules/agenda-card.svelte
@@ -25,14 +25,23 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
-		height: 15rem;
-		width: 25rem;
-		padding: 1rem;
+		height: 12.5rem;
+		width: 20rem;
+		padding: 1rem 0.5rem 0.5rem 1rem;
 		border-radius: 1rem;
 		background-color: hotpink;
 	}
 
 	.button {
 		align-self: flex-end;
+	}
+
+	/* MEDIA QUERY TABLET = 768px */
+	@media (min-width: 48rem) {
+		section {
+			height: 15rem;
+			width: 25rem;
+			padding: 1rem;
+		}
 	}
 </style>
