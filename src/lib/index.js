e537b36cd61ead5d69164200704e748ffd3cf274
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-05-17 15:32:00 +0200

    logo / nav reparatie
    Added our new logo and the navbar was not merged right, so it was still the old code, i made it right agian.

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,7 +5,7 @@ export { default as HeroSlide1 } from '$lib/atoms/heroPictures/heroSlide1.svelte
 export { default as HeroSlide2 } from '$lib/atoms/heroPictures/heroSlide2.svelte';
 export { default as HeroSlide3 } from '$lib/atoms/heroPictures/heroSlide3.svelte';
 
-export { default as Plantswaplogo } from '$lib/atoms/logo/plantswaplogo.svelte';
+export { default as Logo } from '$lib/atoms/logo/logo.svelte';
 export { default as MenuIcon } from '$lib/atoms/icons/menu.svelte';
 export { default as CloseIcon } from '$lib/atoms/icons/close-menu.svelte';
 
