15512d88d97b1471e111074f4c9457eca8d39a89
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-05-19 17:07:28 +0200

    articels aangemaakt

--- src/lib/organisms/slide-cards.svelte
+++ src/lib/organisms/slide-cards.svelte
@@ -4,18 +4,36 @@
 
 <section>
 
-    <article>
+    <article class="card-stekjes">
         <h2>Onze stekjes bieb</h2>
     </article>
-    <article>
+    <article class="card-zaden">
         <h2>Onze zaden bieb</h2>
     </article>
-    <article>
+    <article class="card-gevel">
         <h2>Onze geveltuinen</h2>
     </article>
 
 </section>
 
 <style>
+
+    section{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    section article{
+        width: 90vw;
+        border-radius: 2em;
+        height: 40em;
+    }
+
+    .card-stekjes{
+        background-color: var(--background-color-light);
+        color: var(--)
+    }
 
 </style>
