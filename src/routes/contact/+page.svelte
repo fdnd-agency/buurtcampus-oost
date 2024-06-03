5542c74d63cf507d443b13cb3a7510ccacd015ba
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-06-03 12:12:15 +0200

    Added contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,3 +7,21 @@
 <HeroHeaders 
     heroInfo="{data.heroHeaders[5]}"
 />
+
+<main>
+    <section>
+        <h2>Contact</h2>
+    </section>
+</main>
+
+<style>
+    main{
+        height: 90vh;
+        padding: 2em;
+    }
+    section{
+        background-color: var(--main-color-beige);
+        width: 100%;
+        height: 100%;
+    }
+</style>
