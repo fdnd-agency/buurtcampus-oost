7f49a005f2b87a00de7bab195aac90bb31dfdcba
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2024-09-25 15:07:55 +0200

    Agenda-carousel: Added: Content stijling

--- src/lib/molecules/agenda-carousel.svelte
+++ src/lib/molecules/agenda-carousel.svelte
@@ -7,19 +7,21 @@
 <article>
 	{#each data.agendas as agenda}
 		<div class="card">
-			<h2>{agenda.event}</h2>
-			<time>{agenda.date}</time>
-			<time>{agenda.time}</time>
-			<address>{agenda.address}</address>
-			<p>{agenda.price}</p>
-			<!-- <Button href="/agenda" buttonText="Aanmelden" btnClass="btn-beige" svgFill="svg-brown" /> -->
-
+			<div class="content">
+				<h2>{agenda.event}</h2>
+				<time>{agenda.date}</time>
+				<time>{agenda.time}</time>
+				<address>{agenda.address}</address>
+				<p>{agenda.price}</p>
+				<!-- <Button href="/agenda" buttonText="Aanmelden" btnClass="btn-beige" svgFill="svg-brown" /> -->
+			</div>
 			<img src={agenda.cardImage.url} alt="workshop" width="100" />
 		</div>
 	{/each}
 </article>
 
 <style>
+	/* agenda-carousel styling */
 	article {
 		display: flex;
 		z-index: 3;
@@ -37,8 +39,30 @@
 
 	img {
 		width: 25em;
-		height: 15em;
+		height: 20em;
 		object-fit: cover;
 		border-radius: 0.5em;
+	}
+
+	.card::before {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		background-color: var(--main-color-brown);
+		height: 10em;
+		width: 25em;
+		border-bottom-left-radius: 0.5em;
+		border-bottom-right-radius: 0.5em;
+	}
+
+	.content {
+		position: absolute;
+		margin: 1em;
+		bottom: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		z-index: 2;
+		color: var(--main-color-beige);
 	}
 </style>
