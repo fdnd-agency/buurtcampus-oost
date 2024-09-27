d680157e4cf3a5ed94526e59929cc8da40afd5d6
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2024-09-27 10:29:06 +0200

    Agenda-carousel: Change scroll driven animation

--- src/lib/molecules/agenda-carousel.svelte
+++ src/lib/molecules/agenda-carousel.svelte
@@ -85,20 +85,20 @@
 			@keyframes animate-in-and-out {
 				entry 0% {
 					opacity: 0;
-					transform: translateY(100%);
+					transform: translate(100%);
 				}
 				entry 100% {
 					opacity: 1;
-					transform: translateY(0);
+					transform: translateX(0);
 				}
 
 				exit 0% {
 					opacity: 1;
-					transform: translateY(0);
+					transform: translateX(0);
 				}
 				exit 100% {
 					opacity: 0;
-					transform: translateY(-100%);
+					transform: translate(-10%);
 				}
 			}
 		}
