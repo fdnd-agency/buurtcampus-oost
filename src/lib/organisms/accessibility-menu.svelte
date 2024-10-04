08f525dc343eade5d71676de1bcf83656a9b99c2
Anne <144004647+Annevd@users.noreply.github.com>
2024-10-04 11:55:43 +0200

    #139 Added hovers + focus states, organised CSS
    Hovers toegevoegd voor de switches, ook focus states zijn toegevoegd.
    De CSS is geordend en oude classnames zijn weggehaald.

--- src/lib/organisms/accessibility-menu.svelte
+++ src/lib/organisms/accessibility-menu.svelte
@@ -2,43 +2,43 @@
 import {Accessibility, Moon, Translate, FontIncrease} from '$lib/index.js'; //Switch
 </script>
 
-<form aria-label="Accessibility Options">
-    <label for="menu" class="accessibilityIcon">
+<form title="Accessibility Options" aria-label="Accessibility Options">
+    <label for="menu" class="icon">
         <input type="checkbox" id="menu">
          <Accessibility />
     </label>
-    <div class="accessibilityContent">
+    <div class="content">
         <ul>
             <li>
                 <label for="toggle-darkmode">
-                    <div class="label-container">
+                    <div>
                         <Moon/>
                         <span>Dark mode</span>
                     </div>
                     <input type="checkbox" id="toggle-darkmode" role="switch">
                     <span class="state">
                         <span class="container">
-                          <span class="position"> </span>
+                          <span class="position"></span>
                         </span>
                     </span>
                 </label>
             </li>
             <li>
                 <label for="font-size">
-                    <div class="label-container">
+                    <div>
                         <FontIncrease />
                         <span>Grotere letters</span>
                     </div>
                     <input type="checkbox" id="font-size" role="switch">
                     <span class="state">
                         <span class="container">
-                          <span class="position"> </span>
+                          <span class="position"></span>
                         </span>
                     </span>
                 </label>
             </li>
             <li id="google_translate_element">
-                <div class="label-container">
+                <div>
                     <Translate />
                     <span>Vertaal deze website</span>  
                 </div>  
@@ -49,69 +49,7 @@ import {Accessibility, Moon, Translate, FontIncrease} from '$lib/index.js'; //Sw
 
 <style>
 
-label input[role="switch"] {
-  opacity: 0;
-  cursor: pointer;
-}
-
-label input[role="switch"] ~ .state {
-  display: inline-block;
-  user-select: none;
-}
-
-label input[role="switch"] ~ .state > .container {
-  position: relative;
-  top: 4px;
-  display: inline-block;
-  border: 2px solid #919191;
-  background: #919191;
-  width: 40px;
-  height: 20px;
-  border-radius: 11px;
-  transition: 0.1s ease-in-out;
-}
-
-label input[role="switch"] ~ .state > .container > .position {
-  position: absolute;
-  top: 1px;
-  left: 2px;
-  display: inline-block;
-  border-radius: 9px;
-  width: 14px;
-  height: 14px;
-  background: #fff;
-  transition: 0.1s ease-in-out;
-}
-
-label input[role="switch"]:checked ~ .state > .container > .position {
-  left: 20px;
-  background: #fff;
-}
-
-label input[role="switch"]:checked ~ .state > .container {
-  border: 2px solid #005CDC;
-  background: #005CDC;
-}
-
-.accessibilityContent ul {
-        padding: 0.5rem
-}
-
-.accessibilityContent div {
-    display: flex;
-    gap: 1rem;
-}
-
-.accessibilityContent label {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.accessibilityContent label:hover {
-  cursor: pointer;
-}
-
+/* Form Styles */
 form {
     position: fixed;
     top: 20vh;
@@ -120,59 +58,148 @@ form {
     display: flex;
     flex-direction: row-reverse;
     width: calc(14rem + 5rem);
-    transition: left .3s;
+    transition: left 0.3s;
 }
 
 /* Menu is shown when checkbox is checked */
-form:has(.accessibilityIcon input:checked) {
-    transition: left .3s;
+form:has(.icon input:checked) {
     left: -1rem; /* Slide out to show the menu */
+    transition: left 0.3s;
 }
 
-/* Hide the content when checkbox is unchecked */
-form:not(:has(.accessibilityIcon input:checked)) > .accessibilityContent ul {
+/* Hide content when checkbox is unchecked */
+form:not(:has(.icon input:checked)) > .content ul {
     display: none;
-    transition: left .3s;
 }
 
-.accessibilityContent {
-    width: 14rem;
-    height: max-content;
-    
-    background-color: white;
-    border-radius: 0px 0px 10px 0px;
-}
-
-.accessibilityContent ul li {
-    list-style: none;
-    margin: .4rem;
-}
-
-
-.accessibilityIcon {
-    /* display: flex;
-    justify-content: center;
-    
-    margin: 0px; */
-    padding: 5px;
-    
-    width: 4rem;
-    height: 4rem;
-    
-    background-color: white;
-    border-radius: 0px 10px 10px 0px;
-    
-    cursor: pointer;
-}
-
-/* Extra's */
-
-form:not(:has(.accessibilityIcon input:checked)) > .accessibilityContent {
+/* Accessibility menu hidden height */
+form:not(:has(.icon input:checked)) > .content {
     height: 117.781px;
 }
 
+/* Menu toggle checkbox hidden off-screen */
 input[type="checkbox"][id^="menu"] {
     position: absolute;
     left: -50%;
 }
+
+/* Icon container for the menu */
+.icon {
+    padding: 5px;
+    width: 4rem;
+    height: 4rem;
+    background-color: white;
+    border-radius: 0px 10px 10px 0px;
+    cursor: pointer;
+}
+
+/* Focus outline for icon */
+.icon:has(input:focus) {
+    border: 2px solid red;
+}
+
+/* Content styles */
+.content {
+    width: 14rem;
+    height: max-content;
+    background-color: white;
+    border-radius: 0px 0px 10px 0px;
+}
+
+/* List styles inside the content */
+.content ul {
+    padding: 0.5rem;
+}
+
+.content ul li {
+    list-style: none;
+    margin: 0.4rem;
+}
+
+/* Label and interactive elements */
+.content label {
+    display: flex;
+    justify-content: space-between;
+}
+
+/* Focus and hover state for label */
+.content label:hover {
+    cursor: pointer;
+}
+
+.content label:focus-within {
+    outline: 3px solid red;
+    outline-offset: 3px;
+    border-radius: 5px;
+}
+
+/* Container for icons and text in labels */
+.content div {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+
+/* Switch input styles */
+label input[role="switch"] {
+    opacity: 0; /* Hidden but focusable */
+    cursor: pointer;
+}
+
+/* State container for switch */
+label input[role="switch"] ~ .state {
+    display: inline-block;
+    user-select: none;
+}
+
+/* Switch container */
+label input[role="switch"] ~ .state > .container {
+    position: relative;
+    top: 4px;
+    display: inline-block;
+    border: 2px solid #919191;
+    background: #919191;
+    width: 40px;
+    height: 20px;
+    border-radius: 11px;
+    transition: 0.1s ease-in-out;
+}
+
+/* Hover state for switch container */
+label input[role="switch"]:hover ~ .state > .container {
+    border: 2px solid #3D3D3D;
+    background: #3D3D3D;
+}
+
+/* Switch position (knob) */
+label input[role="switch"] ~ .state > .container > .position {
+    position: absolute;
+    top: 1px;
+    left: 2px;
+    display: inline-block;
+    border-radius: 9px;
+    width: 14px;
+    height: 14px;
+    background: #fff;
+    transition: 0.1s ease-in-out;
+}
+
+/* Checked state for switch knob */
+label input[role="switch"]:checked ~ .state > .container > .position {
+    left: 20px;
+    background: #fff;
+}
+
+/* Checked state for switch container */
+label input[role="switch"]:checked ~ .state > .container {
+    border: 2px solid #005CDC;
+    background: #005CDC;
+}
+
+/* Hover state for checked switch */
+label input[role="switch"]:checked:hover ~ .state > .container {
+    border: 2px solid #3D94FF;
+    background: #3D94FF;
+}
+
 </style>
