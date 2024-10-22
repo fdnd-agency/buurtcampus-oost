118798caf8702207fd1ad6ca38618fbf1380a0a6
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2024-10-22 20:28:45 +0200

    delete viewtransition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script>
 	export let data;
 	import { Menu, Footer } from '$lib/index.js';
-	import Viewtransition from '../lib/viewtransition.svelte';
 </script>
-
-<Viewtransition />
 
 <Menu />
 <slot />
