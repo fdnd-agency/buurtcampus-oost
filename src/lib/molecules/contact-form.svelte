eabd41cc37ccc9f0f2006b5173352b61c5c84f6c
Anne <144004647+Annevd@users.noreply.github.com>
2024-12-04 11:36:09 +0100

    #216 added asterix to visually show requirements

--- src/lib/molecules/contact-form.svelte
+++ src/lib/molecules/contact-form.svelte
@@ -1,54 +1,30 @@
 <form action="https://api.web3forms.com/submit" method="POST">
+    
     <input type="hidden" name="access_key" value="25decdef-5f8c-436d-bbf1-9c2af8bda1e4">
+
     <fieldset class="question">
         <legend>Reden voor contact:</legend>
-            <label  for="question">Ik heb een vraag</label>
-            <input
-                class="radio-question"
-                type="radio"
-                id="question"
-                name="contact-reason"
-                value="question"
-                checked
-            />
-            <label for="workshop" >
-                Ik wil mij aanmelden voor een workshop
-            </label>
-            <input
-                class="radio-question"
-                type="radio"
-                id="workshop"
-                name="contact-reason"
-                value="workshop"
-            />
+        <label for="question">Ik heb een vraag</label>
+        <input class="radio-question" type="radio" id="question" name="contact-reason" value="question" checked />
+        <label for="workshop">Ik wil mij aanmelden voor een workshop</label>
+        <input class="radio-question" type="radio" id="workshop" name="contact-reason" value="workshop" />
     </fieldset>
+
     <div class="fields-container">
-        <label for="name">Voornaam:</label>
-        <input type="text" id="name" name="name" placeholder="Voer je voornaam in" required/>
-
-        <label for="lastname">Achternaam:</label>
-        <input type="text" id="lastname" name="lastname"placeholder="Voer je achternaam in" required/>
-
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email" placeholder="Voer je e-mailadres in" required/>
-
-        <label for="message">
-            Stel je vraag of vertel voor welke workshop je je wilt
-            aanmelden!
-        </label>
-
-        <textarea
-            id="message"
-            name="message"
-            cols="30"
-            rows="10"
-            placeholder="Typ hier je bericht"
-            required
-        ></textarea>
+        <label for="name">Voornaam:<span>*</span></label>
+        <input type="text" id="name" name="name" placeholder="Voer je voornaam in" required />
+        
+        <label for="lastname">Achternaam:<span>*</span></label>
+        <input type="text" id="lastname" name="lastname" placeholder="Voer je achternaam in" required />
+        
+        <label for="email">E-mail:<span>*</span></label>
+        <input type="email" id="email" name="email" placeholder="Voer je e-mailadres in" required />
+        
+        <label for="message">Stel je vraag of vertel voor welke workshop je je wilt aanmelden!<span>*</span></label>
+        <textarea id="message" name="message" cols="30" rows="10" placeholder="Typ hier je bericht" required></textarea>
     </div>
 
     <input type="submit" value="Verzenden" />
-
 </form>
 
 
