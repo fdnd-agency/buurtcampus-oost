191b531c1d636fc921e03b4574442c43fbbecc2b
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-01-09 01:46:29 +0100

    Over: delete comment (AccessibilityMenu)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script>
 	export let data;
-	import { Menu, AccessibilityMenu,  Footer } from '$lib/index.js';
+	import { Menu, AccessibilityMenu, Footer } from '$lib/index.js';
 </script>
 
-<Menu/>
-
-<!-- <AccessibilityMenu /> -->
-
+<Menu />
 <slot />
 <Footer {data} />
