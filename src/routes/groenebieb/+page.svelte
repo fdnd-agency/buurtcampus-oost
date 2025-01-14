10c4751ee1ed2f33724462a5f59d6b44a2bbcd6e
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-01-15 00:07:42 +0100

    Groenebieb: added HeroHeaders + IntroHeaders

--- src/routes/groenebieb/+page.svelte
+++ src/routes/groenebieb/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let data;
-	import { HeroHeaders } from '$lib/index.js';
+	import { HeroHeaders, IntroHeaders } from '$lib/index.js';
 </script>
 
 <HeroHeaders heroInfo={data.heroHeaders[7]} />
+<IntroHeaders heroInfo={data.heroHeaders[7]} />
