88003e9589567ab898266a7e423d8d26bb122f3e
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-01-16 20:19:03 +0100

    Imagecarousel: change api

--- src/routes/geveltuin/carousel/+page.server.js
+++ src/routes/geveltuin/carousel/+page.server.js
@@ -1,9 +1,19 @@
-query MyQuery {
-    carouselImages(where: {slug: "geveltuin"}) {
-      id
-      createdAt
-      images {
-        url
-      }
-    }
-  }
+import { gql } from 'graphql-request';
+import { hygraph } from '$lib/utils/hygraph.js';
+
+export const load = async ({ params }) => {
+	let query = gql`
+		query MyQuery {
+			carouselImages(where: { slug: "geveltuin" }) {
+				id
+				createdAt
+				images {
+					url
+				}
+			}
+		}
+	`;
+	const slug = params.slug;
+	let result = await hygraph.request(query, { slug });
+	return result;
+};
