4c35dc9d5b10096757240c888ae286ba58c26296
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-01-19 00:04:37 +0100

    Header: code netjes maken

--- src/lib/molecules/hero-headers.svelte
+++ src/lib/molecules/hero-headers.svelte
@@ -9,42 +9,45 @@
 </header>
 
 <style>
-    .overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 50vh;
-        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.67) 0%, rgba(0, 0, 0, 0.1) 100%);
-    } 
-    header{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    h1{
-        view-transition-name: h1;
-        font-family: var(--header-font);
-        position: absolute;
-        color: var(--text-color-white);
-        font-size: clamp(3em, 15vw, 5em);
-    }
-    img{
-        width: 100%;
-        height: 50vh;
-        object-fit: cover;
-    } 
-    @media (min-width: 48rem) {
-        h1 {
-            font-size: clamp(5.8em, 13vw, 6em);
-        }
-    }
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 50vh;
+		background: linear-gradient(to bottom, rgba(0, 0, 0, 0.67) 0%, rgba(0, 0, 0, 0.1) 100%);
+	}
     
-    @media (min-width: 64rem) {
-        .overlay, img{
-            width: 100%;
-            height: 50vh;
-        } 
-    }
+	header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
+	h1 {
+		font-family: var(--header-font);
+		position: absolute;
+		color: var(--text-color-white);
+		font-size: clamp(3em, 15vw, 5em);
+	}
+
+	img {
+		width: 100%;
+		height: 50vh;
+		object-fit: cover;
+	}
+
+	@media (min-width: 48rem) {
+		h1 {
+			font-size: clamp(5.8em, 13vw, 6em);
+		}
+	}
+
+	@media (min-width: 64rem) {
+		.overlay,
+		img {
+			width: 100%;
+			height: 50vh;
+		}
+	}
 </style>
