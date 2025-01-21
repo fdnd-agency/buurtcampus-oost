cee3809ab0dfddc22269ddaf45599c89740bfa7f
Larissa <larissa.mikkers@hotmail.com>
2025-01-21 14:04:56 +0100

    deleted comment

--- src/routes/zaden/[slug]/+page.svelte
+++ src/routes/zaden/[slug]/+page.svelte
@@ -88,8 +88,7 @@
     object-fit: cover;
     border-radius: var(--border-card);
   }
-  
-  /* temporary fix */
+
   @media screen and (max-width: 375px) {
     .stekje-container {
       grid-template-columns: 1fr;
