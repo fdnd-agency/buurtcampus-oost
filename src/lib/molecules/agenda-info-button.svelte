5ee7d0ac6a3b9219eb0f255ee7168a74feac58ab
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-01-22 22:23:34 +0100

    Agendabutton: icon center

--- src/lib/molecules/agenda-info-button.svelte
+++ src/lib/molecules/agenda-info-button.svelte
@@ -49,6 +49,9 @@
 		cursor: pointer;
 		margin-bottom: 1em;
 		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		background-color: var(--main-color-brown);
 	}
 
