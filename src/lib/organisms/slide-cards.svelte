8f019c65c0d24a89d258d1d23557384540d4e283
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-02-11 17:39:51 +0100

    refactor: Verbetert de semantische structuur :recycle:

--- src/lib/organisms/slide-cards.svelte
+++ src/lib/organisms/slide-cards.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <section>
-	<article>
-		{#each data.cardSlides as cardSlide}
+	{#each data.cardSlides as cardSlide}
+		<article>
 			<Dot svgFill="var(--main-color-orange)" />
 			<h2>{cardSlide.titleCard}</h2>
 			<p>{cardSlide.paragraphCard}</p>
@@ -17,7 +17,7 @@
 					height="982"
 					loading="lazy"
 					src={cardSlide.imageCard.url}
-					alt="{cardSlide.imageCardAltText}"
+					alt={cardSlide.imageCardAltText}
 				/>
 			</picture>
 
@@ -27,6 +27,6 @@
 				buttonClass={cardSlide.buttonStyles.buttonClass}
 				svgFill={cardSlide.buttonStyles.svgFill}
 			/>
-		{/each}
-	</article>
+		</article>
+	{/each}
 </section>
