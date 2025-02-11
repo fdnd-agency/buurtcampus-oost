47b06c132dbd62c7033fb13bb2d518847b82fa13
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-02-11 17:13:19 +0100

    refactor: nieuwe code overgezet naar oude code file :fire:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,10 @@
 <script>
 	export let data;
 	import { HeroSlider, SlideCards, Agenda, HomeCampus, HomePartners } from '$lib/index.js';
-	import Slidecards1 from '$lib/organisms/slidecards1.svelte';
-
-	
 </script>
 
-	<HeroSlider {data} />
-	<SlideCards {data} />
-	<Slidecards1 {data} />
-	<HomeCampus {data} />
-	<Agenda {data} />
-	<HomePartners {data} />
-
-	
+<HeroSlider {data} />
+<SlideCards {data} />
+<HomeCampus {data} />
+<Agenda {data} />
+<HomePartners {data} />
