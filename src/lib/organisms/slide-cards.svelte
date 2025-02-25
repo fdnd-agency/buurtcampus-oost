af05333cc1f2288731242ea3f93013f2fc84c952
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-02-25 12:56:39 +0100

    refactor: mobile first :recycle:

--- src/lib/organisms/slide-cards.svelte
+++ src/lib/organisms/slide-cards.svelte
@@ -1,26 +1,27 @@
 <script>
 	export let data;
-	import { Button, Dot } from '$lib/index.js';
+	import { Button, Dot, CardslidesContent } from '$lib/index.js';
 </script>
 
-<section>
+<section class="card-container">
 	{#if data?.cardSlides?.length}
 		{#each data.cardSlides as cardSlide}
-			<article>
-				<Dot svgFill="var(--main-color-orange)" />
-				<h2>{cardSlide.titleCard}</h2>
-				<p>{cardSlide.paragraphCard}</p>
-				<p>{cardSlide.paragraphCard2}</p>
+			<article class={cardSlide.cardStyles.backgroundColor}>
+				<Dot svgFill={cardSlide.svgStyles.svgFill} />
+				<CardslidesContent
+					titleCard={cardSlide.titleCard}
+					titleColor={cardSlide.cardStyles.titleColor}
+				/>
 
 				<picture>
-					<img
-						width="783"
-						height="982"
-						loading="lazy"
-						src={cardSlide.imageCard.url}
-						alt={cardSlide.imageCardAltText}
-					/>
+					<img width="783" height="982" loading="lazy" src={cardSlide.imageCard.url} alt="" />
 				</picture>
+
+				<CardslidesContent
+					paragraphCard={cardSlide.paragraphCard}
+					paragraphCard2={cardSlide.paragraphCard2}
+					paragraphColor={cardSlide.cardStyles.paragraphColor}
+				/>
 
 				<Button
 					href={cardSlide.buttonStyles.href}
@@ -36,6 +37,67 @@
 </section>
 
 <style>
+	.card-container {
+		container-type: inline-size;
+		container-name: slidecards;
+	}
+
+	article {
+		align-items: center;
+		position: relative;
+		padding: 2em 1em;
+		bottom: 3em;
+		margin: 0 auto;
+		z-index: 1;
+		border-radius: var(--border-bigCard);
+	}
+
+	article::before {
+		content: url(/assets/leaf-orange.svg);
+		position: absolute;
+		width: 20em;
+		height: 20em;
+		left: -4em;
+		transform: rotate(135deg);
+		z-index: -1;
+	}
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		border-radius: var(--border-bigCard);
+	}
+
+	@container slidecards (min-width: 64rem) {
+		article::before {
+			width: 26em;
+			height: 26em;
+		}
+
+		img {
+			height: 100%;
+			width: 32em;
+		}
+	}
+
+	/* backgroundColor variants */
+	.bg-green {
+		background-color: var(--card-color-green);
+	}
+
+	.bg-beige {
+		background-color: var(--main-color-beige);
+	}
+
+	.bg-brown {
+		background-color: var(--card-color-brown);
+	}
+
+	.bg-orange {
+		background-color: var(--card-color-orange);
+	}
+
 	.error-message {
 		font-size: 1.5rem;
 		color: var(--main-color-red);
